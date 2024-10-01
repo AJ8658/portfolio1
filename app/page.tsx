223f6880a1a6019744a0b86643de0fdf0eac21c4
AJ8658 <ajadvw7@icloud.com>
2024-10-01 13:51:04 +0000

    implemented Approach, Clients, Recent Approach & Experience and UI with corresponding elements

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,25 @@
 import Grid from "../components/Grid";
 import Hero from "../components/Hero";
+import RecentProjects from "../components/RecentProjects";
 import { FloatingNav } from "../components/ui/FloatingNav";
 import { FaHome } from 'react-icons/fa'; // Import the home icon
+import { navItems } from "../data";
+import Clients from "../components/Clients";
+import Expierence from "../components/Expierence";
+import Approach from "../components/Approach";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-x-hidden mx-auto sm:px-10 px-5 min-h-screen">
       <div className="max-w-7xl w-full">
         {/* Correctly passing navItems as an array of objects */}
-        <FloatingNav 
-          navItems={[
-            { name: "Home", link: "/", icon: <FaHome /> }
-          ]}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
+        <RecentProjects />
+        <Clients />
+        <Expierence />
+        <Approach />
       </div>
     </main>
   );
